79e8d6ed7d52716681d17fd5f7e381a795009608
Calleb Bezerra <calleb.bezerra@gmail.com>
2021-06-23 16:18:59 -0300

    estudos para prova

---
+++ java/Empresa/src/empresa/Empregado.java
@@ -0,0 +1,35 @@
+package empresa;
+
+
+public class Empregado {
+    private String nome;
+    private double salarioMensal;
+    private double corrigeSalario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        if (salarioMensal < 0) {
+            corrigeSalario = Math.abs(salarioMensal);
+            this.salarioMensal = corrigeSalario;
+        } else {
+        this.salarioMensal = salarioMensal;
+        }
+    }
+    
+    public double calcularSalarioAnual(double salario) {
+        double anual = (salario * 12);
+        return anual;
+        
+    }
+}
---
+++ java/Empresa/src/empresa/Empresa.java
@@ -0,0 +1,37 @@
+
+package empresa;
+
+import java.util.Scanner;
+
+public class Empresa {
+
+    public static void main(String[] args) {
+        Empregado empregado1 = new Empregado();
+        Empregado empregado2 = new Empregado();
+        Scanner input = new Scanner(System.in);
+        String nome;
+        double salario;
+        
+        System.out.print("Digite o nome do Funcionário 1: ");
+        nome = input.next();
+        empregado1.setNome(nome);
+        System.out.print("Digite o valor do seu salário: ");
+        salario = input.nextDouble();
+        empregado1.setSalarioMensal(salario);
+        
+        System.out.print("Digite o nome do Funcionário 2: ");
+        nome = input.next();
+        empregado2.setNome(nome);
+        System.out.print("Digite o valor do seu salário: ");
+        salario = input.nextDouble();
+        empregado2.setSalarioMensal(salario);
+        
+        System.out.println("O salário anual de " 
+                + empregado1.getNome() + " é: " 
+                + empregado1.calcularSalarioAnual(empregado1.getSalarioMensal()));
+        
+        System.out.println("O salário anual de " 
+                + empregado2.getNome() + " é: " 
+                + empregado2.calcularSalarioAnual(empregado2.getSalarioMensal()));
+    }
+}
