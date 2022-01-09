a2c28a5077817eba7cf709778a793074d093f89c
Calleb Bezerra <calleb.bezerra@gmail.com>
2022-01-09 13:28:55 -0300

    Aulas de PHP Alura

--- php-alura/avancando/listas.php
+++ php-alura/avancando/listas.php
@@ -4,3 +4,12 @@ $idadeList = [21, 23, 19, 25, 30, 41, 18];
 $umaIdade = $idadeList[0];
 
 echo $umaIdade;
+
+// Para adicionar um item no último índice, basta informar o valor:
+    // Exemplo:
+
+$idadeList[] = 20;
+
+foreach ($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
