83917b085e03e2ed95c19f525cafdd58ef8f352f
Calleb Bezerra <53497665+callebbs@users.noreply.github.com>
2022-02-12 16:52:43 -0300

    Update index.php

--- php-alura/php_mysql/blog/index.php
+++ php-alura/php_mysql/blog/index.php
@@ -1,5 +1,14 @@
 <?php
 
+$mysql = new mysqli('localhost', 'root', '', 'blog');
+$mysql->set_charset('UTF-8');
+
+if($mysql == TRUE) {
+    echo "Banco conectado.";
+} else {
+    echo "Erro na conexão.";
+}
+
 include 'Artigo.php';
 $artigo = new Artigo();
 $artigos = $artigo->exibirTodos();
