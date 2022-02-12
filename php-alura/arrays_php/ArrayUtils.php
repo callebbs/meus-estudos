ad1f6b85a7350a955b9854e94324931f131e7d9c
Calleb Bezerra <53497665+callebbs@users.noreply.github.com>
2022-02-12 14:59:01 -0300

    Manipulando Arrays com PHP - Alura

--- php-alura/arrays_php/ArrayUtils.php
+++ php-alura/arrays_php/ArrayUtils.php
@@ -1,14 +1,26 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types=1);
+
+namespace Alura;
 
 class ArrayUtils
 {
-    public static function remover(int $elemento, array &$array)
-    {
+
+    public static function remover (int $elemento, array $array) {
         $posicao = array_search($elemento, $array, true);
-        if (is_int($posicao)) {
+        if(is_int($posicao)) {
             unset($array[$posicao]);
         } else {
-            echo "Nao foi encontrado no array";
+            echo "Não foi encontrado no array.";
         }
+    }
+
+    public static function encontrarPessoasComSaldoMaior(int $saldo, array $array): array {
+        $correntistasComSaldoMaior = array();
+        foreach ($array as $chave => $valor) {
+            if($valor > $saldo) {
+                $correntistasComSaldoMaior[] = $chave;
+            }
+        }
+        return $correntistasComSaldoMaior;
     }
 }
--- php-alura/arrays_php/index.php
+++ php-alura/arrays_php/index.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace Alura;
+
+require 'autoload.php';
+
+/*
 $correntistas = [
     'Giovanni',
     'João',
@@ -20,3 +25,56 @@ $correntistasPagantes = array_diff($correntistas, $correntistasNaoPagantes);
 echo "<pre>";
 var_dump($correntistasPagantes);
 echo "</pre>";
+*/
+
+$correntistas = [
+    'Giovanni',
+    'João',
+    'Maria',
+    'Luis',
+    'Luisa',
+    'Rafael'
+];
+
+$saldos = [
+    2500,
+    3000,
+    4400,
+    1000,
+    8700,
+    9000
+];
+
+
+$array_associativo = [
+    "Giovanni" => 2500,
+    "João" => 3000,
+    "Maria" => 4000
+];
+
+$relacionados = array_combine($correntistas, $saldos);
+
+
+$relacionados["Matheus"] = 4500;
+
+#echo "O saldo do Giovanni é: {$relacionados["Giovanni"]}" . PHP_EOL;
+
+if (array_key_exists("João",$relacionados)) {
+    echo "O saldo do João é: {$relacionados["João"]}";
+} else {
+    echo "Chave não encontrada.";
+}
+
+$mariores = ArrayUtils::encontrarPessoasComSaldoMaior(3000, $relacionados);
+
+echo "<pre>";
+var_dump($maiores);
+echo "</pre>";
+
+/*
+Fatal error: Uncaught Error: Call to undefined method Alura\ArrayUtils::encontrarPessoasComSaldoMaior() 
+in E:\GitHub\meus-estudos\php-alura\arrays_php\index.php:68 Stack trace: #0 {main} thrown 
+in E:\GitHub\meus-estudos\php-alura\arrays_php\index.php on line 68
+*/
+
+#Não sei o que deu :(
